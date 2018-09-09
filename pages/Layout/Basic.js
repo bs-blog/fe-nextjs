78966ef1e4c0b49ced7e13f5bbfb1dd66a8c27c5
waheng chang <wahengchang@gmail.com>
2018-09-09 08:50:20 +0800

    Added drawer

--- pages/Layout/Basic.js
+++ pages/Layout/Basic.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Layout } from 'antd'
 import css from './Basic.css'
+import Drawer from './Drawer'
 
 const { Header, Footer, Sider, Content } = Layout
 
@@ -8,7 +9,8 @@ export default class App extends React.Component {
   render() {
     return (
       <Layout>
-        <Sider>Sider</Sider>
+        {/* <Sider>Sider</Sider> */}
+        <Drawer />
         <Layout>
           <Header>Header</Header>
           <Content className={css.layoutContentWrapper}>{this.props.children}</Content>
