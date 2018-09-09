c0359f4e64b7450bbc371f4687b10707ce9abe4f
StevenLeeTW <leejirlu@gmail.com>
2018-09-10 01:24:54 +0800

    refine drawer

--- pages/Layout/Drawer.js
+++ pages/Layout/Drawer.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import css from './Drawer.css'
 
-import { Layout, Menu, Icon } from 'antd'
+import { Layout, Menu, Icon, Drawer, Button } from 'antd'
 const { Sider } = Layout
 const SubMenu = Menu.SubMenu
 
@@ -23,53 +23,61 @@ export default class DrawerComponent extends React.Component {
   render() {
     return (
       <div className={css.MenuDrawer}>
-        <a onClick={this.showDrawer}>
-          <Icon type="menu-unfold" theme="outlined" />
-        </a>
-        {this.state.visible && (
-          <Sider>
-            <div className="logo" />
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-              <Menu.Item key="1">
-                <Icon type="pie-chart" />
-                <span>Option 1</span>
-              </Menu.Item>
-              <Menu.Item key="2">
-                <Icon type="desktop" />
-                <span>Option 2</span>
-              </Menu.Item>
-              <SubMenu
-                key="sub1"
-                title={
-                  <span>
-                    <Icon type="user" />
-                    <span>User</span>
-                  </span>
-                }
-              >
-                <Menu.Item key="3">Tom</Menu.Item>
-                <Menu.Item key="4">Bill</Menu.Item>
-                <Menu.Item key="5">Alex</Menu.Item>
-              </SubMenu>
-              <SubMenu
-                key="sub2"
-                title={
-                  <span>
-                    <Icon type="team" />
-                    <span>Team</span>
-                  </span>
-                }
-              >
-                <Menu.Item key="6">Team 1</Menu.Item>
-                <Menu.Item key="8">Team 2</Menu.Item>
-              </SubMenu>
-              <Menu.Item key="9">
-                <Icon type="file" />
-                <span>File</span>
-              </Menu.Item>
-            </Menu>
-          </Sider>
-        )}
+        <Button type={'primary'} onClick={this.showDrawer}>
+          <Icon type={'menu'} />
+        </Button>
+        <Drawer
+          title="Basic Drawer"
+          placement="left"
+          closable={false}
+          onClose={this.onClose}
+          visible={this.state.visible}
+          style={{
+            top: 36,
+            position: 'absolute'
+          }}
+        >
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="user" />
+                  <span>User</span>
+                </span>
+              }
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <Icon type="team" />
+                  <span>Team</span>
+                </span>
+              }
+            >
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9">
+              <Icon type="file" />
+              <span>File</span>
+            </Menu.Item>
+          </Menu>
+        </Drawer>
       </div>
     )
   }
