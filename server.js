a5b44844dc039a17cb6baae3a41dd78818e0f80e
StevenLeeTW <leejirlu@gmail.com>
2018-09-13 01:07:33 +0800

    rm redundant code

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const next = require('next')
-const { parse } = require('url')
 
 const dev = process.env.NODE_ENV !== 'production'
 const PORT = process.env.PORT || 3000
 
-const app = next({ dir: '.', dev })
+const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
