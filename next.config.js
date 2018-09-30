6d4c52c66a576118a354a7e8cb4fa993e30620af
waheng chang <wahengchang@gmail.com>
2018-09-30 18:23:43 +0800

    Added seo meta header

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 const withCSS = require('@zeit/next-css')
-module.exports = withCSS({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: '[local]___[hash:base64:5]'
+module.exports = {
+  ...withCSS({
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: '[local]___[hash:base64:5]'
+    }
+  }),
+  publicRuntimeConfig: {
+    PRODUCT_AUTHOR: process.env.PRODUCT_AUTHOR,
+    PRODUCT_NAME: process.env.PRODUCT_NAME,
+    PRODUCT_HOST: process.env.PRODUCT_HOST
   }
-})
+}
