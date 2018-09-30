fc50581c7b5b404829c4ccadf7a6871606f75320
waheng chang <wahengchang@gmail.com>
2018-09-30 12:26:24 +0800

    data source control

--- src/lib/dao/StoryList.js
+++ src/lib/dao/StoryList.js
@@ -2,10 +2,8 @@ const Base = require('./Base.js')
 
 const storyParser = item => {
   const rawContent = JSON.parse(item.data)
-  const name = rawContent.blocks[0].text
   return {
     ...item,
-    name,
     rawContent
   }
 }
