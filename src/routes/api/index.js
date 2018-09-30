d93f85c3001be4aad74edb7ff3275947f2beee46
waheng chang <wahengchang@gmail.com>
2018-09-30 12:24:52 +0800

    Added source control on api router

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
+const { normalize } = require('../middleware/normalize')
 
+router.use(normalize)
 router.use('/storys', require('./storys'))
 router.use('/categorys', require('./categorys'))
 router.use('/authors', require('./authors'))
