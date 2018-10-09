4171fc3527bd6c9101bba4082c33e4a3ccaee820
waheng chang <wahengchang@gmail.com>
2018-10-09 23:34:35 +0800

    refactoring ui color

--- pages/Layout/Footer.js
+++ pages/Layout/Footer.js
@@ -17,7 +17,7 @@ export default class FooterComponent extends React.Component {
   render() {
     // const { config } = this.props
     return (
-      <Footer className={css.footerWrapper} style={{ background: 'black' }}>
+      <Footer className={css.footerWrapper} style={{ background: 'rgba(0, 29, 29,1)' }}>
         <div className={css.contentWrapper}>
           <span>Copyright © 2018 yakyunews. </span>
           <span> 專業日本職棒新聞、評論、以及所有日本棒球資訊網站 </span>
